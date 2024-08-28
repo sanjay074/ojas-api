e8c66a39db3eee82913176bb8b79333339527964
Durgesh Gaur <durgesh@uveoustech.com>
2024-08-28 16:36:13 +0530

    add new api

--- User/routes/class.js
+++ User/routes/class.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {verifyTokenAndAdmin,verifyTokenAndUser}= require("../../middlewares/auth");
-const {addClass,getAllClass,updateClass,geOneClass,getAllClassByCourseId} = require("../controllers/classController"); 
+const {addClass,getAllClass,updateClass,geOneClass,getAllClassByCourseId,deleteClassList} = require("../controllers/classController"); 
 
 router.post("/addClass",verifyTokenAndAdmin,addClass);
 router.get("/getAllClass",verifyTokenAndAdmin,getAllClass);
 router.put("/updateClass/:id",verifyTokenAndAdmin,updateClass);
 router.get("/geOneClass/:id",verifyTokenAndAdmin,geOneClass);
 router.get("/classes/:courseId",getAllClassByCourseId);
+router.delete('/deleteClassList/:id', verifyTokenAndAdmin,deleteClassList);
 
 module.exports = router;
